ed4124d0aa59bdaca1d546fd248a949c415b405a
wangyu <wangyu28@staff.sina.cn>
2020-05-12 09:29:50 +0800

    h

--- src/model/server.h
+++ src/model/server.h
@@ -19,19 +19,19 @@
 namespace xflow {
 class Server {
  public:
-  Server() {
-    server_w_ = new ps::KVServer<float>(0);
-    server_w_->set_request_handle(FTRL::KVServerFTRLHandle_w());
-    //server_w_->set_request_handle(SGD::KVServerSGDHandle_w());
+    Server() {
+        server_w_ = new ps::KVServer<float>(0);
+        server_w_->set_request_handle(FTRL::KVServerFTRLHandle_w());
+        //server_w_->set_request_handle(SGD::KVServerSGDHandle_w());
 
-    server_v_ = new ps::KVServer<float>(1);
-    server_v_->set_request_handle(FTRL::KVServerFTRLHandle_v());
-    //server_v_->set_request_handle(SGD::KVServerSGDHandle_v());
-    std::cout << "init server success " << std::endl;
-  }
-  ~Server() {}
-  ps::KVServer<float>* server_w_;
-  ps::KVServer<float>* server_v_;
+        server_v_ = new ps::KVServer<float>(1);
+        server_v_->set_request_handle(FTRL::KVServerFTRLHandle_v());
+        //server_v_->set_request_handle(SGD::KVServerSGDHandle_v());
+        std::cout << "init server success " << std::endl;
+    }
+    ~Server() {}
+    ps::KVServer<float>* server_w_;
+    ps::KVServer<float>* server_v_;
 };
-}  // namespace xflow
-#endif  // SRC_MODEL_SERVER_H_
+}    // namespace xflow
+#endif    // SRC_MODEL_SERVER_H_
