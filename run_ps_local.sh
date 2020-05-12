8024586c377ab444ef51b2f7b41b23da7bb940b6
shiduo <shiduo@staff.sina.cn>
2020-05-12 11:38:29 +0800

    h

--- run_ps_local.sh
+++ run_ps_local.sh
@@ -3,7 +3,7 @@ echo $root_path
 model_name=1
 epochs=10
 workernum=5
-python mapfeat.py
+python mapfeat.py $workernum
 #sh ./scripts/local.sh 1 1 $root_path/build/test/src/xflow_lr $root_path/data/small_train $root_path/data/small_train $model_name $epochs
 sh ./scripts/local.sh 1 $workernum $root_path/build/test/src/xflow_lr $root_path/data/train.libsvm $root_path/data/test.libsvm $model_name $epochs
 
