1666cc8c0b702c2d1302fcac8ba02c60ac0f79bd
shiduo <shiduo@staff.sina.cn>
2020-05-19 11:24:49 +0800

    h

--- run_ps_local.sh
+++ run_ps_local.sh
@@ -10,10 +10,10 @@ modelname=$1.$tmp
 modeltype=1
 epochs=5
 workernum=5
-testflag=1
-python mapfeat.py $workernum $testflag
+testflag=$2
+#python mapfeat.py $workernum $testflag
 #sh ./scripts/local.sh 1 1 $root_path/build/test/src/xflow_lr $root_path/data/small_train $root_path/data/small_train $model_name $epochs
-sh ./scripts/local.sh 1 $workernum $root_path/build/test/src/xflow_lr $root_path/data/train.libsvm $root_path/data/test.libsvm $modeltype $epochs $modelname $workernum
+sh ./scripts/local.sh 1 $workernum $root_path/build/test/src/xflow_lr $root_path/data/small_train $root_path/data/small_train $modeltype $epochs $modelname $workernum
 
 #cat model/model.$modelname.* |sort -k 1n -u> model/model.all
 #wc -l model/model.all
